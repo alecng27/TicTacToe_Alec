a33d55fa740fb0fdcb24f28f8c1ac82028b7f873
alecng27 <alec.nguyen27@gmail.com>
2023-03-11 15:08:54 -0600

    update

--- Play.java
+++ Play.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Play {
     public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
-        game.play();
+
+        Scanner scanner = new Scanner(System.in);
+        String playAgain = "y";
+
+        while (playAgain.equalsIgnoreCase("y")) {
+
+            TicTacToe game = new TicTacToe();
+            game.play();
+            
+            System.out.println("Do you want to play again? (y/n)");
+            playAgain = scanner.nextLine();
+
+            if (playAgain.equalsIgnoreCase("n")) {
+                System.out.println("-------Thank you for playing at Alec's!-------");
+            }
+        }
+        scanner.close();
     }
 }
